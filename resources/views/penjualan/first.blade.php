f981c465b135dd28aa150385248c0eb37e32482a
Your Name <ardhiyashita2002@gmail.com>
2021-11-22 13:03:03 +0800

    saving to github

---
+++ resources/views/penjualan/first.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts/master')
+
+@section('title', 'First')
+
+@section('content')
+    HELLOOOOOOOOOOOOOOOOOOO
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,13 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
+    return view('layouts/master');
+});
+
+Route::get('/welcome', function () {
     return view('welcome');
+});
+
+Route::get('/first', function () {
+    return view('penjualan/first');
 });
