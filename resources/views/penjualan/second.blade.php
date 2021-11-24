80d58d9a52e1cf15e2da41eb10147401df5ced47
Your Name <ardhiyashita2002@gmail.com>
2021-11-24 20:57:00 +0800

    saving to github

--- resources/views/penjualan/second.blade.php
+++ resources/views/penjualan/second.blade.php
@@ -14,6 +14,7 @@
                         <th class="border-top-0">HARGA</th>
                         <th class="border-top-0">KETERANGAN</th>
                         <th class="border-top-0">ACTION</th>
+                        <th class="border-top-0">ACTION</th>
                     </tr>
                 </thead>
             </table>
