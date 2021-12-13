12304864cc1b1cdddf114944b5da07d4615c6ce2
Willy <willyputra02@gmail.com>
2021-12-13 15:23:28 +0800

    kategori produk update

--- resources/views/kategori/index.blade.php
+++ resources/views/kategori/index.blade.php
@@ -22,12 +22,6 @@
         <div class="card shadow mb-4">
             <div class="card-header py-3 d-sm-flex align-items-center justify-content-between mb-4">
                 <h6 class="m-0 font-weight-bold text-primary d-flex">List Master Data Kategori Produk</h6>
-                    {{-- <div class="btn-group">
-                        <a href="{{ route('produk-block') }}" class="d-none d-sm-inline-block btn btn-sm btn-primary shadow-sm mr-2"><i
-                            class="fas fa-square fa-sm text-white-50"></i>  Block Items</a>
-                        <a href="{{ route('produk-list') }}" class="d-none d-sm-inline-block btn btn-sm btn-primary shadow-sm"><i
-                            class="fas fa-list fa-sm text-white-50"></i>  List Items</a>
-                    </div> --}}
             </div>
             <div class="card-body">
                 <div class="table-responsive">
